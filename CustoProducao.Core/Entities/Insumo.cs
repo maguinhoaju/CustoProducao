3fd5ec3583c253a7fecbc4aea2845c197638367e
Carlos Magno de França Veiga <maguinho.aju@gmail.com>
2018-08-09 22:23:45 -0300

    Nova Arquitetura

---
+++ CustoProducao.Core/Entities/Insumo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustoProducao.Core
+{
+    public partial class Insumo
+    {
+        public Guid IdInsumo { get; set; }
+        public DateTime DtInsumoCadastro { get; set; }
+        public string NmInsumo { get; set; }
+        public string DsInsumo { get; set; }
+        public decimal VlInsumoNota { get; set; }
+        public decimal VlInsumoFrete { get; set; }
+    }
+}
---
+++ CustoProducao.Infrastructure/CustoProducaoDbContext.cs
@@ -0,0 +1,61 @@
+﻿using CustoProducao.Core;
+using CustoProducao.Infrastructure.DAL.EntityConfiguration;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustoProducao.Infrastructure
+{
+    public partial class CustoProducaoDbContext : DbContext
+    {
+        public CustoProducaoDbContext()
+        {
+        }
+
+        public CustoProducaoDbContext(DbContextOptions<CustoProducaoDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //DO NOTHING
+        }
+
+        public virtual DbSet<Insumo> Insumos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new InsumoConfiguration());
+
+            //modelBuilder.Entity<Insumo>(entity =>
+            //{
+            //    entity.HasKey(e => e.IdInsumo);
+
+            //    entity.ToTable("TB_INSUMO");
+
+            //    entity.Property(e => e.IdInsumo)
+            //        .HasColumnName("ID_INSUMO")
+            //        .ValueGeneratedNever();
+
+            //    entity.Property(e => e.DsInsumo)
+            //        .IsRequired()
+            //        .HasColumnName("DS_INSUMO")
+            //        .HasMaxLength(1000)
+            //        .IsUnicode(false);
+
+            //    entity.Property(e => e.DtInsumoCadastro)
+            //        .HasColumnName("DT_INSUMO_CADASTRO")
+            //        .HasColumnType("datetime");
+
+            //    entity.Property(e => e.NmInsumo)
+            //        .IsRequired()
+            //        .HasColumnName("NM_INSUMO")
+            //        .HasMaxLength(100)
+            //        .IsUnicode(false);
+
+            //    entity.Property(e => e.VlInsumoFrete).HasColumnName("VL_INSUMO_FRETE");
+
+            //    entity.Property(e => e.VlInsumoNota).HasColumnName("VL_INSUMO_NOTA");
+            //});
+        }
+    }
+}
